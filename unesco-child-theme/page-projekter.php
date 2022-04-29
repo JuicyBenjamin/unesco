624fcae14c7932d6a097e78b6cb709886808dec1
Benjamin Vinterberg <benj924c@stud.kea.dk>
2022-04-29 10:59:42 +0200

    yolo

---
+++ unesco-child-theme/page-projekter.php
@@ -0,0 +1,105 @@
+<head>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/projekter.css">
+</head>
+
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+
+<section id="prosjekt_innledning">
+  <div class="innledning_bilde">
+    <img
+      src="https://vinterfjell.dk/kea/09_cms/unesco/wp-content/uploads/2022/04/oily-bird-wikipedia-1024x681-2.webp"
+      alt=""
+    />
+    <div class="innledning_bilde_grid">
+      <img
+        src="https://vinterfjell.dk/kea/09_cms/unesco/wp-content/uploads/2022/04/ocean.webp"
+        alt=""
+        />
+      <img
+        src="https://vinterfjell.dk/kea/09_cms/unesco/wp-content/uploads/2022/04/pexels-photo-416528.webp"
+        alt=""
+      />
+    </div>
+  </div>
+        
+  <div class="innledning_tekst">
+    <h2>Generation verdensmål</h2>
+    <div class="innledning_info">
+      <p>
+        <b>Fokus</b> UNESCO Verdensmålsskoler <br />
+        <b>Uddannelsestrin</b> Grundskole <br />
+        <b>Skolenavn</b> Lindbjergskolen <br />
+        <b>Kontakt</b> Betina Moesby, beti0470@dblit.dk <br />
+      </p>
+    </div>
+
+    <p>
+      I dette forløb vil vi arbejde med, hvordan vi gør verdensmålet nr.
+      16: ”Fred, retfærdighed og stærke institutioner” kan blive til
+      hverdagsmål.
+    </p>
+
+    <p>
+      Vi vil se nærmere på verdensmålet, og de undermål det indeholder.
+    </p>
+    
+    <p>
+      Eleverne vil komme til at arbejde med følgende produkter: En podcast
+      på engelsk i flere afsnit En plakat, der passer til podcastens
+      afsnit Digte og billeder, der vil være selvlysende Opbygningen af et
+      mørkerum til vores udstilling Statements, der fænger
+    </p>
+
+    <p>Verdensmål nummer 16: Fred Retfærdighed og stærke institutioner</p>
+    <p>
+      Vi vil arbejde med udgangspunkt i det samfund, vi ser i dag, og vi
+      vil se fremad mod utopien, men vi vil også hæfte op på det tidligere
+      projekt, hvor vi arbejdede med ”Rammer vi dystopien, hvis vi
+      fortsætter som nu”
+    </p>
+    <p>
+      Desuden vil eleverne komme til at: <br> • At lave en positiv
+      demonstration for fred, retfærdighed og stærke institutioner. <br />
+      • At arbejde med LEAPS-protokoller <br />
+      • At give og modtage feedback <br />
+      • At kunne fortælle publikum om projektet
+    </p>
+  </div>
+
+</section>
+
+<section id="prosjekt_main">
+  <div class="main_tekst">
+    <p>Til lærere:</p>
+    <p>Eleverne formår at se verdensmål nummer 16 fra mange forskellige vinkler, især fra deres. De når at gå i dybden med verdensmål nummer 16.</p>
+    <p>
+      <ul>
+        <li>At lave en podcast om verdensmål nummer 16 på engelsk</li>
+        <li>At lave en rigtig plakat</li>
+        <li>At arbejde med digte, billeder og udtryk både som inspiration og provokation.</li>
+        <li>At arbejde med et mørkerum for at få effekter på vores udstilling</li>
+        <li>Arbejde med lys og refleksioner</li>
+        <li>Arbejde med film produktion</li>
+      </ul>
+    </p>
+  </div>
+  
+  <div class="projekt_main_bilde">
+    <img src="https://vinterfjell.dk/kea/09_cms/unesco/wp-content/uploads/2022/04/vand1-2.webp" alt="">
+  </div>
+</section>
+
+<?php
+get_footer();
